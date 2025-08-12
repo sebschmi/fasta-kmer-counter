4cce54951f3b7e6ce313200abd3e7e06735dd3e1
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2025-08-12 11:38:39 +0300

    Implement CLI.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::ffi::{OsStr, OsString};
+
+use clap::Parser;
+
+#[derive(clap::Parser)]
+struct Cli {
+    #[clap(index = 1)]
+    input: OsString,
+}
+
+#[tokio::main]
+async fn main() {
+    let cli = Cli::parse();
+
+    let total_count = count_path(&cli.input).await;
+    println!("{total_count}");
+}
+
+async fn count_path(path: &OsStr) -> usize {
+    todo!()
 }
