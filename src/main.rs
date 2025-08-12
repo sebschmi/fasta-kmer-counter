f7f08feb88586fae595d39cf75915b24a34e96a2
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2025-08-12 12:59:44 +0300

    Basic implementation.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,146 @@
-use std::ffi::{OsStr, OsString};
+use std::{
+    path::{Path, PathBuf},
+    pin::Pin,
+};
 
+use async_recursion::async_recursion;
 use clap::Parser;
+use tokio::{
+    fs::{File, ReadDir, read_dir},
+    io::{AsyncRead, AsyncReadExt, BufReader},
+};
+use tokio_stream::StreamExt;
+use tokio_tar::Archive;
 
 #[derive(clap::Parser)]
 struct Cli {
+    #[clap(short, long)]
+    k: usize,
+
     #[clap(index = 1)]
-    input: OsString,
+    input: PathBuf,
 }
 
 #[tokio::main]
 async fn main() {
     let cli = Cli::parse();
 
-    let total_count = count_path(&cli.input).await;
+    let total_count = count_path(&cli.input, cli.k).await;
     println!("{total_count}");
 }
 
-async fn count_path(path: &OsStr) -> usize {
-    todo!()
+async fn count_path(path: impl AsRef<Path>, k: usize) -> usize {
+    let path = path.as_ref();
+    if let Ok(directory) = read_dir(path).await {
+        count_directory(directory, k).await
+    } else if let Ok(file) = File::open(path).await {
+        count_file(file, k).await
+    } else {
+        // If everything fails, just ignore it.
+        0
+    }
+}
+
+#[async_recursion]
+async fn count_directory(mut directory: ReadDir, k: usize) -> usize {
+    let mut sum = 0;
+
+    while let Ok(Some(entry)) = directory.next_entry().await {
+        sum += count_path(entry.path(), k).await;
+    }
+
+    sum
+}
+
+#[allow(clippy::manual_unwrap_or_default, clippy::manual_unwrap_or)]
+async fn count_file(mut file: impl AsyncRead + Unpin + Send + Sync, k: usize) -> usize {
+    if let Some(amount) = count_tar_file(&mut file, k).await {
+        amount
+    } else if let Some(amount) = count_fasta_file(&mut file, k).await {
+        amount
+    } else {
+        // If everything fails, just ignore it.
+        0
+    }
+}
+
+fn count_tar_file<'file>(
+    file: impl 'file + AsyncRead + Unpin + Send + Sync,
+    k: usize,
+) -> Pin<Box<dyn 'file + Future<Output = Option<usize>> + Send>> {
+    Box::pin(async move {
+        let mut archive = Archive::new(file);
+        let Ok(mut entries) = archive.entries() else {
+            return None;
+        };
+        let mut sum = 0;
+
+        while let Some(Ok(file)) = entries.next().await {
+            sum += count_file(file, k).await;
+        }
+
+        if sum == 0 {
+            // It is unclear how to discover if the file is actually a tar archive.
+            // So we simply say that if the tokio-tar crate finds no file, or no file that would be fasta, then it is not a tar file.
+            None
+        } else {
+            Some(sum)
+        }
+    })
+}
+
+async fn count_fasta_file(file: impl AsyncRead + Unpin + Send + Sync, k: usize) -> Option<usize> {
+    let mut reader = BufReader::with_capacity(1024 * 1024, file);
+    let mut sum = 0;
+
+    // Skip to entry header start.
+    let mut last_is_newline = true;
+    while let Ok(b) = reader.read_u8().await {
+        if last_is_newline && b == b'>' {
+            // Found entry header.
+            break;
+        }
+
+        last_is_newline = b == b'\n' || b == b'\r';
+    }
+
+    loop {
+        // Skip entry header.
+        let mut has_characters = false;
+        while let Ok(b) = reader.read_u8().await {
+            has_characters = true;
+            if b == b'\n' {
+                break;
+            }
+        }
+
+        if !has_characters {
+            break;
+        }
+
+        // Count entry characters.
+        let mut character_count = 0usize;
+        let mut last_is_newline = true;
+        while let Ok(b) = reader.read_u8().await {
+            if b == b'>' {
+                if last_is_newline {
+                    // Found entry header.
+                    break;
+                } else {
+                    // Found entry header character  without preceding newline.
+                    return None;
+                }
+            }
+
+            if b != b'\n' && b != b'\r' {
+                character_count += 1;
+            }
+
+            last_is_newline = b == b'\n' || b == b'\r';
+        }
+
+        sum += character_count - k + 1;
+    }
+
+    Some(sum)
 }
